08cd7382be51b075f930b1e752f83086605dd325
Daniel Bartolini <djb3149@gmail.com>
2023-01-06 18:02:22 -0500

    added wildcard pathing in app.get

--- server.js
+++ server.js
@@ -39,9 +39,13 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
-})
+});
 
 app.get('/api/notes', (req, res) => {
     console.info(`${req.method} request recieved!`);
